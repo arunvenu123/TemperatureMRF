62e626eab26cd0f3a2f938e75f8c472ceedc17e8
arun <arunvenuv@yahoo.com>
2020-06-09 02:01:48 +0530

    08/06/2020

--- app/src/main/java/com/example/temperaturemrf/MainActivity.java
+++ app/src/main/java/com/example/temperaturemrf/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.temperaturemrf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView date = findViewById(R.id.date);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+        String currentDate = sdf.format(new Date());
+        date.setText(currentDate);
+
+
     }
 }
