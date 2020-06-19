465f1938d9c123884eaab67ddccbee66749e0d60
Arun Venu <arunvenuv@yahoo.com>
2020-06-19 12:12:44 +0530

    from lap

--- app/src/main/java/com/example/temperaturemrf/MainActivity.java
+++ app/src/main/java/com/example/temperaturemrf/MainActivity.java
@@ -9,12 +9,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -53,7 +58,26 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addToDatabase();
+                DatabaseReference reference =FirebaseDatabase.getInstance().getReference().child("Data");
+                Query query = reference.orderByChild("employeeno").equalTo(empnum.getText().toString().trim()).orderByChild("date").equalTo(currentDate);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists()){
+                            updateEntry();
+                        }
+                        else
+                            addToDatabase();
+                    }
+
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+
             }
         });
 
@@ -67,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
         String inTempText = inTemperature.getSelectedItem().toString().trim();
         String outTempText = outTemperature.getSelectedItem().toString().trim();
 
-        if (!TextUtils.isEmpty(empnumberText) && !TextUtils.isEmpty(busTempText) && !TextUtils.isEmpty(recorderText)) {
+        if (!TextUtils.isEmpty(empnumberText) && !TextUtils.isEmpty(recorderText)) {
             String id = dbTemp.push().getKey();
 
             entry dataentry = new entry(currentDate, empnumberText, busTempText, recorderText, inTempText, outTempText);
@@ -77,6 +101,10 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Enter all fields", Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void updateEntry(){
+        Toast.makeText(this,"Same employee num",Toast.LENGTH_LONG).show();
     }
 
 }
